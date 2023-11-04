5d2930f6d43674f97523d2a9a402d033f2527de1
Nikolas Freitas <nikolasbvf@gmail.com>
2023-11-04 17:03:26 -0300

    :tada: Hello world

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class MainTest {
+
+    @Test
+    void test() {
+        Assertions.assertEquals(2, 1+1);
+    }
+}
